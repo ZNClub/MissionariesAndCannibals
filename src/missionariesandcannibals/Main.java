15e4bc6529c46e8249d4ddba66d9b338c9b2d495
Leon D'souza <LeonDsouza@users.noreply.github.com>
2016-09-19 11:30:21 +0530

    Output Commit

--- src/missionariesandcannibals/Main.java
+++ src/missionariesandcannibals/Main.java
@@ -75,3 +75,32 @@ public class Main {
 		}
 	}
 }
+/*
+Output :
+Test BFS : 
+==== Missionaries and Cannibals Problem ====
+Choose the search method: 
+	 1. Breadth-first search
+	 2. Depth-limited search
+
+Type your choice and press ENTER: 1
+
+Solution (cannibalLeft,missionaryLeft,boat,cannibalRight,missionaryRight): 
+(3,3,L,0,0) -> (1,3,R,2,0) -> (2,3,L,1,0) -> (0,3,R,3,0) -> (1,3,L,2,0) -> (1,1,R,2,2) -> (2,2,L,1,1) -> (2,0,R,1,3) -> (3,0,L,0,3) -> (1,0,R,2,3) -> (1,1,L,2,2) -> (0,0,R,3,3)
+Depth: 11
+BUILD SUCCESSFUL (total time: 7 seconds)
+
+Test Depth Limited Search : 
+==== Missionaries and Cannibals Problem ====
+Choose the search method: 
+	 1. Breadth-first search
+	 2. Depth-limited search
+
+Type your choice and press ENTER: 2
+
+Solution (cannibalLeft,missionaryLeft,boat,cannibalRight,missionaryRight): 
+(3,3,L,0,0) -> (1,3,R,2,0) -> (3,3,L,0,0) -> (1,3,R,2,0) -> (3,3,L,0,0) -> (1,3,R,2,0) -> (3,3,L,0,0) -> (1,3,R,2,0) -> (3,3,L,0,0) -> (1,3,R,2,0) -> (2,3,L,1,0) -> (0,3,R,3,0) -> (1,3,L,2,0) -> (1,1,R,2,2) -> (2,2,L,1,1) -> (2,0,R,1,3) -> (3,0,L,0,3) -> (1,0,R,2,3) -> (1,1,L,2,2) -> (0,0,R,3,3)
+Depth: 19
+BUILD SUCCESSFUL (total time: 2 seconds)
+
+*/
